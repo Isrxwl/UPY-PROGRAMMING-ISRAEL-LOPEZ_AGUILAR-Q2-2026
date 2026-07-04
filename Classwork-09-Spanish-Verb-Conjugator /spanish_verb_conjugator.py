96641def1b7b814609e142afe41e317aa20f059c
Isrxwl <2509114@upy.edu.mx>
2026-07-03 21:41:21 -0600

    Implement Spanish verb conjugation feature
    Added functionality to conjugate Spanish verbs based on user input.

--- Classwork-09-Spanish-Verb-Conjugator /spanish_verb_conjugator.py
+++ Classwork-09-Spanish-Verb-Conjugator /spanish_verb_conjugator.py
@@ -1,1 +1,22 @@
+# INPUT
+verbo = input("Ingrese verbo: ")
 
+# PROCESS
+pronombres = ['yo', 'tu', 'el', 'nosotros', 'vosotros', 'ellos']
+terminaciones = {
+    'ar': ['o', 'as', 'a', 'amos', 'ais', 'an'],
+    'er': ['o', 'es', 'e', 'emos', 'eis', 'en'],
+    'ir': ['o', 'es', 'e', 'imos', 'is', 'en']
+}
+
+# Obtener la raíz (todo menos las últimas 2 letras) y la terminación (últimas 2 letras)
+raiz = verbo[:-2]
+terminacion = verbo[-2:]
+
+# Buscar la lista de terminaciones correspondiente en el diccionario
+terminaciones_correspondientes = terminaciones[terminacion]
+
+# OUTPUT
+# Ciclo sobre los pronombres usando el mismo índice para ambas listas
+for i in range(len(pronombres)):
+    print(pronombres[i] + " " + raiz + terminaciones_correspondientes[i])
